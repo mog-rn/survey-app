5f71f24c517120a77407f9739c7524a225f63227
Mogakamo <mogaka.amo254@gmail.com>
2022-08-15 16:49:38 +0300

    build: resposes page with hardcoded data

--- src/components/Responses.tsx
+++ src/components/Responses.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import React from "react";
+
+function createData(
+    surveyId: number,
+    question1: string,
+    question2: string,
+    question3: string,
+) {
+
+    return {surveyId, question1, question2, question3}
+}
+
+const rows = [
+    createData(1, 'Answer1', 'Answer3', 'Answer2')
+]
 
 const Responses = () => {
   return (
-    <div>Responses</div>
-  )
-}
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Survey ID</TableCell>
+            <TableCell>Question 1</TableCell>
+            <TableCell>Question 2</TableCell>
+            <TableCell>Question 3</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            {rows.map((row) => (
+                <TableRow>
+                    <TableCell>{row.surveyId}</TableCell>
+                    <TableCell>{row.question1}</TableCell>
+                    <TableCell>{row.question2}</TableCell>
+                    <TableCell>{row.question3}</TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
-export default Responses
+export default Responses;

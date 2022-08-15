8e5996cb056f561bcb886e6b94c1ef77ce5732c6
Mogakamo <mogaka.amo254@gmail.com>
2022-08-15 15:12:22 +0300

    build: ui is set

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from "react";
 import { Button, Box, Typography } from "@mui/material";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -11,15 +10,20 @@ const Home = () => {
         flexDirection: "column",
         justifyItems: "space-between",
         alignItems: "center",
+        padding: "220px",
       }}
     >
-      <Typography variant="h3">Welcome to this Survey App</Typography>
-      <Button variant="contained"><Link to="survey">Take the Survey</Link></Button>
-      <Button variant="contained" color="success">
-        <Link to="responses">View your response</Link>
+      <Typography variant="h3" sx={{ marginBottom: "10px" }}>
+        Welcome to this Survey App
+      </Typography>
+      <Button variant="contained" sx={{ marginBottom: "10px" }}>
+        <Link to="survey">Take the Survey</Link>
+      </Button>
+      <Button variant="contained" color="success" sx={{ marginBottom: "10px" }}>
+        <Link to="responses" sx={{color: 'white'}}>View your response</Link>
       </Button>
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

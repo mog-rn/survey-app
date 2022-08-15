d98f2a1407a7e85038e58f0f859a00d24e36a7e7
Mogakamo <mogaka.amo254@gmail.com>
2022-08-15 13:09:57 +0300

    build: Added the server files

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,16 @@
-import express from "express"
-import path from "path"
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 
-const app = express()
+const app: Express = express();
+const port = process.env.PORT;
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Éxpress + Typescript server");
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at port ${port}`);
+    
+})
